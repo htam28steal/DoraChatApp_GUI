ff450462db868a355ecba3cde6068734cc59914b
quangkid2212 <163752892+hckq2212@users.noreply.github.com>
2025-04-18 23:20:38 +0700

    Create GroupsScreen

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ import ResetPasswordStep1 from './Screens/ResetPasswordStep1';
 import ResetPasswordStep2 from './Screens/ResetPasswordStep2';
 import useSocketListeners from './hooks/useSocketListeners';
 import Conversations from './Screens/ConversationScreen'
+import Groups from './Screens/GroupsScreen'
 
 const Stack = createStackNavigator();
 
@@ -97,6 +98,12 @@ function AppContainer() {
         <Stack.Screen
           name="ResetPasswordStep2Screen"
           component={ResetPasswordStep2}
+          options={{ headerShown: false }}
+
+        />
+         <Stack.Screen
+          name="GroupsScreen"
+          component={Groups}
           options={{ headerShown: false }}
 
         />
